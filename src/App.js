1c6a518d3e2abf6311e4a922b393af0045c410cf
nbriannl <neilbrian.nl@gmail.com>
2018-01-09 17:39:12 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
@@ -13,8 +14,42 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <h1>
+          Header
+        </h1>
+
+        <div class="container-fluid text-center">
+          <h1><small>Welcome to </small>NUS Events</h1>
+        </div>
+
+        <div>
+          <h4>What type of Events do you want to see?</h4>
+        </div>
+
+        <p> <button type="button">Post New Event</button> </p>
+
+        <ul>
+          < ButtonListItem buttonText="hello there!">apple</ ButtonListItem>
+          < ButtonListItem buttonText="hello there!">carrot</ ButtonListItem>
+          < ButtonListItem buttonText="hello there!">orange</ ButtonListItem>
+          < ButtonListItem buttonText="hello there!">banana</ ButtonListItem>
+          < ButtonListItem buttonText="hello there!">papaya</ ButtonListItem>
+        </ul>
+
       </div>
     );
+  }
+}
+
+class ButtonListItem extends Component {
+  render() {
+    return (
+      <li>
+        <button>{this.props.buttonText}</button>
+        {this.props.children}
+      </li>
+    )
   }
 }
 
