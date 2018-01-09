cfdf477f5db6948d55462c73a8f440add03a5efc
nbriannl <neilbrian.nl@gmail.com>
2018-01-09 17:41:33 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'semantic-ui-css/semantic.min.css';
+import "./App.css";
+import { Button, Card, List, Input, Checkbox, Icon } from 'semantic-ui-react'
 
+class ToDoItem extends Component {
+  constructor(props) {
+    super(props)
+    this.onCheckboxChange = this.onCheckboxChange.bind(this);
+  }
 
-class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <h1>
-          Header
-        </h1>
-
-        <div class="container-fluid text-center">
-          <h1><small>Welcome to </small>NUS Events</h1>
-        </div>
-
-        <div>
-          <h4>What type of Events do you want to see?</h4>
-        </div>
-
-        <p> <button type="button">Post New Event</button> </p>
-
-        <ul>
-          < ButtonListItem buttonText="hello there!">apple</ ButtonListItem>
-          < ButtonListItem buttonText="hello there!">carrot</ ButtonListItem>
-          < ButtonListItem buttonText="hello there!">orange</ ButtonListItem>
-          < ButtonListItem buttonText="hello there!">banana</ ButtonListItem>
-          < ButtonListItem buttonText="hello there!">papaya</ ButtonListItem>
-        </ul>
-
-      </div>
+      <List.Item>
+        <Checkbox
+          label={this.props.todo.text}
+          checked={this.props.todo.checked}
+          onChange={this.onCheckboxChange}
+        />
+      </List.Item>
     );
+  }
+
+  onCheckboxChange(event, data) {
+    this.props.onChange(data.checked, this.props.index)
   }
 }
 
-class ButtonListItem extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          text: "apple",
+          checked: false,
+        },
+        {
+          text: "orange",
+          checked: false,
+        },
+      ],
+    };
+    this.onCheckboxChange = this.onCheckboxChange.bind(this);    
+  }
+
   render() {
     return (
-      <li>
-        <button>{this.props.buttonText}</button>
-        {this.props.children}
-      </li>
+      <div className="App Aligner">
+        <Card color='red' className='Aligner-item toDoCard'>
+          <Card.Content>
+            <Card.Header>To Do List <Icon name='list' /></Card.Header>
+            <Card.Meta>A list of things to do</Card.Meta>
+            <Card.Description>This is a list of things to do.</Card.Description>
+            <Input fluid action='Add Item' placeholder='Insert Item To Do...' />
+            <List divided relaxed>
+              {this.state.todos.map((todo, index) => {
+                return (
+                  <ToDoItem 
+                    todo={todo}
+                    index={index}
+                    onChange={this.onCheckboxChange} 
+                  />
+                );
+              })}
+            </List>
+          </Card.Content>
+        </Card>
+      </div>
     )
+  }
+
+  onCheckboxChange(value, index) {
+    // get old value
+    let todos = this.state.todos;
+    // get value of specific todo we want
+    let todo = todos[index];
+    // modify the old values with our new values
+    todos[index] = {text: todo.text, checked: value };
+    // set the new todos as the new state.
+    this.setState({ todos: todos });
+
   }
 }
 
