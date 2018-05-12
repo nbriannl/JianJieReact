4b2b53928477aeaf773269616636ae2a8aa16380
nbriannl <neilbrian.nl@gmail.com>
2018-05-12 22:20:04 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -3,56 +3,115 @@ import 'semantic-ui-css/semantic.min.css';
 import "./App.css";
 import { Button, Card, List, Input, Checkbox, Icon, Grid } from 'semantic-ui-react'
 
-class ToDoItem extends Component {
+class App extends Component {
   constructor(props) {
-    super(props)
-    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
+    super(props);
+
+    const listOne = <ToDoList cardColor='green' /> 
+    const listTwo = <ToDoList cardColor='blue' /> 
+    this.state = {
+        todolists: [],
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick() {
+    let todolists = this.state.todolists;
+    let colors = ["red", "orange", "yellow", "olive", "green", "teal", "blue", "violet", "purple", "pink", "brown", "grey", "black"];
+    let color = colors[this.state.todolists.length % colors.length];
+    const addedList = <ToDoList cardColor={color} />     
+    todolists.push(addedList)
+    this.setState({ todolists: todolists });
   }
 
   render() {
     return (
-      <List.Item>
-        <Grid columns={2}>
-          <Grid.Row>
-            <Grid.Column width={15} verticalAlign='middle'>
-              <Checkbox
-                label={this.props.todo.text}
-                checked={this.props.todo.checked}
-                onChange={this.handleCheckboxChange}
-              />
-            </Grid.Column>
-            <Grid.Column width={1}>
-              <Button
-                negative circular size='tiny' icon='remove circle'
-                onClick={this.handleItemDelete}
-              />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </List.Item>
-    );
-  }
-
-  handleItemDelete(event, data) {
-    this.props.onDelete(this.props.index);
-  }
-
-  handleCheckboxChange(event, data) {
-    //corresponds to TodoList.handleCheckboxChange(value, index) 
-    this.props.onChange(data.checked, this.props.index)
+      <div className="App Aligner">
+      <Button className="Aligner-item" onClick={this.handleOnClick}>Add</Button>
+        {this.state.todolists.map(function mapTodoLists(todoList) {
+          return <div className="Aligner-item toDoList">{todoList}</div>;
+        })}
+        {/* todolist => <div className="Aligner-item toDoList">{todolist}</div>)} */}
+      </div>
+    )
   }
 }
 
-function EmptyToDoListItem(props) {
-  return (
-    <List.Item>
-      <Checkbox
-        label='Empty list! Add an item.'
-        disabled
-      />
-    </List.Item>
-  )
+class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+    };
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleItemSubmit = this.handleItemSubmit.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
+  }
+
+  render() {
+    const isTodoListEmpty = (this.state.todos.length === 0) ? true : false;
+    let listItems = null;
+    if (isTodoListEmpty) {
+      listItems = <EmptyToDoListItem />
+    } else {
+      listItems = this.state.todos.map((todo, index) => {
+        return (
+          <ToDoItem
+            todo={todo}
+            index={index}
+            key={index + 'tomato'}
+            onChange={this.handleCheckboxChange}
+            onDelete={this.handleItemDelete}
+          />
+        );
+      })
+    }
+
+    return (
+      <div>
+        <Card color={this.props.cardColor} >
+          <Card.Content>
+            <Card.Header>To Do List <Icon name='list' /></Card.Header>
+            <Card.Meta>A list of things to do</Card.Meta>
+            <AddItemField onSubmit={this.handleItemSubmit} />
+            <List divided relaxed>
+              {listItems}
+            </List>
+          </Card.Content>
+        </Card>
+      </div>
+    )
+  }
+
+  handleItemDelete(index) {
+    // get old value
+    let todos = this.state.todos;
+    // modify the old value by remove the specfic value
+    if (index > -1) {
+      todos.splice(index, 1);
+    }
+    // set the new todos as new state
+    this.setState({ todos: todos });
+  }
+
+  handleItemSubmit(item) {
+    // alert(item + ' is being added!');
+    let todos = this.state.todos;
+    todos.push({ text: item, checked: false })
+    this.setState({ todos: todos });
+  }
+
+  handleCheckboxChange(value, index) {
+    // get old value
+    let todos = this.state.todos;
+    // get value of specific todo we want
+    let todo = todos[index];
+    // modify the old values with our new values
+    todos[index] = { text: todo.text, checked: value };
+    // set the new todos as the new state.
+    this.setState({ todos: todos });
+
+  }
 }
 
 class AddItemField extends Component {
@@ -122,97 +181,61 @@ class AddItemField extends Component {
   }
 }
 
-class ToDoList extends Component {
+class ToDoItem extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      todos: [
-      ],
-    };
+    super(props)
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
-    this.handleItemSubmit = this.handleItemSubmit.bind(this);
     this.handleItemDelete = this.handleItemDelete.bind(this);
   }
 
   render() {
-    const isTodoListEmpty = (this.state.todos.length === 0) ? true : false;
-    let listItems = null;
-    if (isTodoListEmpty) {
-      listItems = <EmptyToDoListItem />
-    } else {
-      listItems = this.state.todos.map((todo, index) => {
-        return (
-          <ToDoItem
-            todo={todo}
-            index={index}
-            key={index + 'tomato'}
-            onChange={this.handleCheckboxChange}
-            onDelete={this.handleItemDelete}
-          />
-        );
-      })
-    }
-
     return (
-      <div className="Aligner-item" >
-        <Card color={this.props.cardColor} >
-          <Card.Content>
-            <Card.Header>To Do List <Icon name='list' /></Card.Header>
-            <Card.Meta>A list of things to do</Card.Meta>
-            <AddItemField onSubmit={this.handleItemSubmit} />
-            <List divided relaxed>
-              {listItems}
-            </List>
-          </Card.Content>
-        </Card>
-      </div>
-    )
+      <List.Item>
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column width={15} verticalAlign='middle'>
+              <Checkbox
+                label={this.props.todo.text}
+                checked={this.props.todo.checked}
+                onChange={this.handleCheckboxChange}
+              />
+            </Grid.Column>
+            <Grid.Column width={1}>
+              <Button
+                negative circular size='tiny' icon='remove circle'
+                onClick={this.handleItemDelete}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </List.Item>
+    );
   }
 
-  handleItemDelete(index) {
-    console.log('handleItemDeleteFunction');
-    // get old value
-    let todos = this.state.todos;
-    // get value of specific todo to delete
-    let todoToRemove = todos[index]
-    // modify the old value by remove the specfic value
-    if (index > -1) {
-      todos.splice(index, 1);
-    }
-    // set the new todos as new state
-    this.setState({ todos: todos });
+  handleItemDelete(event, data) {
+    this.props.onDelete(this.props.index);
   }
 
-  handleItemSubmit(item) {
-    // alert(item + ' is being added!');
-    let todos = this.state.todos;
-    todos.push({ text: item, checked: false })
-    this.setState({ todos: todos });
-  }
-
-  handleCheckboxChange(value, index) {
-    // get old value
-    let todos = this.state.todos;
-    // get value of specific todo we want
-    let todo = todos[index];
-    // modify the old values with our new values
-    todos[index] = { text: todo.text, checked: value };
-    // set the new todos as the new state.
-    this.setState({ todos: todos });
-
+  handleCheckboxChange(event, data) {
+    //corresponds to TodoList.handleCheckboxChange(value, index) 
+    this.props.onChange(data.checked, this.props.index)
   }
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App Aligner">
-        <ToDoList cardColor='green' />
-        <br />
-        <ToDoList cardColor='yellow'/>
-      </div>
-    )
-  }
+function EmptyToDoListItem(props) {
+  return (
+    <List.Item>
+      <Checkbox
+        label='Empty list! Add an item.'
+        disabled
+      />
+    </List.Item>
+  )
 }
+
+
+
+
+
 
 export default App;
